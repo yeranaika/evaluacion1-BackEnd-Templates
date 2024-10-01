e13fe2ddf8fa7865054280d897220085bf7f18fc
yeranaika <darca604@gmail.com>
2024-09-30 23:40:20 -0300

    v4
    templates y css listos

--- evaluacion1/home/views.py
+++ evaluacion1/home/views.py
@@ -7,11 +7,12 @@ def index(request):
     return render(request, 'index.html')  # Asegúrate de que 'index.html' esté en 'evaluacion1/templates/'
 
 def Galeria(request):
-    contenido = {
-        'titulo': 'Página Secundaria',
-        'descripcion': 'Esta es una página secundaria que incluye imágenes y enlaces.',
+    contenido1 = {
+        'titulo': 'Galeria',
+        'descripcion': '//////// Tatuajes realizados :P ////////',
     }
-    return render(request, 'Galeria.html', contenido)  # Debe estar en 'evaluacion1/templates/'
+    return render(request, 'Galeria.html', contenido1)  # Debe estar en 'evaluacion1/templates/'
+    
 
 def contacto(request):
     return render(request, 'contacto.html')  # Debe estar en 'evaluacion1/templates/'
